591f4e3547e83661cacc51c34867bbe935bec621
itgmi <123205197+itgmi@users.noreply.github.com>
2023-03-11 12:05:29 -0600

    Update create_csf.py

--- custom_csf/models/create_csf.py
+++ custom_csf/models/create_csf.py
@@ -142,7 +142,7 @@ class CreateCsf(models.TransientModel):
                             "\n")[0]
                         if entidad_value:
                             state = self.env['res.country.state'].search([
-                                ('name', 'like', entidad_value.title()),
+                                ('name', '=', entidad_value.title()),
                                 ('country_id', '=', country.id)],
                                 limit=1)
                             record.state_id = state.id
