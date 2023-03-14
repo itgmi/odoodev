99756afd54aedd5a41cf67a53f9ebb09ea8b5232
Your Name <you@example.com>
2023-03-14 23:53:46 +0530

    ADD [Location]

--- mercadolibre/models/available_stock.py
+++ mercadolibre/models/available_stock.py
@@ -10,23 +10,26 @@ class AvailableStock(models.Model):
     free_qty = fields.Float()
 
     def sample_function(self):
-        stock = self.env['product.product'].search([])
+        stock = self.env['stock.quant'].search([])
         available = self.env['available.stock'].search([])
         vals = []
         for rec in available:
             vals.append(rec.product_id.id)
         for recs in stock:
-            if recs.id not in vals:
-                self.env['available.stock'].create({
-                    'product_id': recs.id,
-                    'free_qty': recs.free_qty,
-                })
-            else:
+            if recs.location_id.usage == 'internal':
                 record = self.env['available.stock'].search([
-                    ('product_id', '=', recs.id)])
-                record.write({
-                    'free_qty': recs.free_qty,
-                })
+                    ('product_id', '=', recs.product_id.id),
+                    ('location_id', '=', recs.location_id.id)])
+                if record:
+                    record.write({
+                        'free_qty': recs.available_quantity,
+                    })
+                else:
+                    self.env['available.stock'].create({
+                        'product_id': recs.product_id.id,
+                        'location_id': recs.location_id.id,
+                        'free_qty': recs.available_quantity,
+                    })
         return {
             'name': 'Available Quantity',
             'view_type': 'tree',
